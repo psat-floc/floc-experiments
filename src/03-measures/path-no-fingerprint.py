19f0720919dba6029a5b7eacdefeec93b9e34265
charlypoirier <pro.poiriercharly@gmail.com>
2021-12-16 10:09:51 +0100

    removed division by 0

--- src/03-measures/path-no-fingerprint.py
+++ src/03-measures/path-no-fingerprint.py
@@ -47,7 +47,10 @@ for years in counter:
         if(counter[years][key] == 1):
             nb_identified_users += 1
         
-    avg = (nb_identified_users / len_data) * 100
+    if len_data > 0:
+        avg = (nb_identified_users / len_data) * 100
+    else:
+        avg = 0
     plot_writer.writerow([avg])
     print(years + ": " + str(avg))
 
